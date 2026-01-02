1e87e761d2ea0dc8c9d2e34aba671ec0d8c90c06
Yashosw10 <yashosw10@gmail.com>
2026-01-03 00:16:23 +0530

    change in env file

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/auth.controllers.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-//import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-//router.use(arcjetProtection);
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,13 +6,13 @@ import {
   sendMessage,
 } from "../controllers/message.controllers.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-//import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
 // the middlewares execute in order - so requests get rate-limited first, then authenticated.
 // this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
-//router.use(arcjetProtection, protectRoute);
+router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);

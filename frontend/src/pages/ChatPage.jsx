a2009091dcf9932881bb172bdc78cff709a34371
Yashosw10 <yashosw10@gmail.com>
2026-01-07 16:39:07 +0530

    fix: update import statements to include file extensions for consistency refactor: add a blank line for improved readability in useChatStore

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,12 +1,12 @@
 import { useChatStore } from "../store/useChatStore";
 
-import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
-import ProfileHeader from "../components/ProfileHeader";
-import ActiveTabSwitch from "../components/ActiveTabSwitch";
-import ChatsList from "../components/ChatsList";
-import ContactList from "../components/ContactList";
-import ChatContainer from "../components/ChatContainer";
-import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer.jsx";
+import ProfileHeader from "../components/ProfileHeader.jsx";
+import ActiveTabSwitch from "../components/ActiveTabSwitch.jsx";
+import ChatsList from "../components/ChatsList.jsx";
+import ContactList from "../components/ContactList.jsx";
+import ChatContainer from "../components/ChatContainer.jsx";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder.jsx";
 
 function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
